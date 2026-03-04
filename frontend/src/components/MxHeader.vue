ee7128bfd7725096eceda783819bc8cd2c675dc1
Max <scorpiomax90@gmail.com>
2026-03-04 14:05:09 +0200

    frontend: Update navbar

--- frontend/src/components/MxHeader.vue
+++ frontend/src/components/MxHeader.vue
@@ -1,40 +1,39 @@
 <template>
   <div class="p-4">
-    <van-config-provider theme="dark">
-      <van-nav-bar
-        title="Lift Tracker"
-        left-text="Назад"
-        right-text="Сегодня"
-      />
+    <van-nav-bar
+      title="Lift Tracker"
+      left-text="Menu"
+      :right-text="formattedDate"
+      @click-right="showCalendar = true"
+    />
 
-      <van-calendar
-        v-model:show="showCalendar"
-        v-model="selectedDate"
-        :min-date="minDate"
-        :max-date="maxDate"
-        @confirm="onConfirm"
-        @close="showCalendar = false"
-        color="#1989fa"
-      />
+    <van-calendar
+      v-model:show="showCalendar"
+      v-model="selectedDate"
+      :min-date="minDate"
+      :max-date="maxDate"
+      @confirm="onConfirm"
+      @select="onSelect"
+      @close="showCalendar = false"
+      color="#3c8ee0"
+    />
 
-      <van-cell
-        title="Выбрать дату тренировки"
-        :value="formattedDate"
-        is-link
-        @click="showCalendar = true"
-      />
+    <!-- <van-cell
+      title="Выбрать дату тренировки"
+      is-link
+      @click="showCalendar = true"
+    /> -->
 
-      <van-form v-if="selectedDate">
-        <van-field
-          v-model="workoutName"
-          label="Название тренировки"
-          placeholder="Например: Ноги + пресс"
-        />
-        <van-button type="primary" block @click="saveWorkout"
-          >Сохранить</van-button
-        >
-      </van-form>
-    </van-config-provider>
+    <!-- <van-form v-if="selectedDate">
+      <van-field
+        v-model="workoutName"
+        label="Название тренировки"
+        placeholder="Например: Ноги + пресс"
+      />
+      <van-button type="primary" block @click="saveWorkout"
+        >Сохранить</van-button
+      >
+    </van-form> -->
   </div>
 </template>
 
@@ -43,7 +42,7 @@ import { ref, computed } from 'vue';
 
 import { Locale } from 'vant';
 import enUS from 'vant/es/locale/lang/en-US';
-import { showToast } from 'vant';
+import { showToast, showSuccessToast } from 'vant';
 
 Locale.use('en-US', enUS);
 
@@ -53,11 +52,10 @@ const minDate = ref(new Date(2025, 0, 1));
 const maxDate = ref(new Date(2030, 11, 31));
 
 const formattedDate = computed(() => {
-  if (!selectedDate.value) return 'Не выбрано';
+  if (!selectedDate.value) return 'Today';
   return selectedDate.value.toLocaleDateString('en-US', {
     day: 'numeric',
     month: 'long',
-    year: 'numeric',
   });
 });
 
@@ -66,7 +64,22 @@ const workoutName = ref('');
 const onConfirm = (date) => {
   selectedDate.value = date;
   showCalendar.value = false;
-  showToast(`Выбрана дата: ${date.toLocaleDateString('en-EN')}`);
+  showSuccessToast(
+    `${date.toLocaleDateString('en-EN', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    })}`,
+  );
+};
+
+const onSelect = (date) => {
+  console.log(date);
+  if (selectedDate.value && selectedDate.value.getTime() === date.getTime()) {
+    onConfirm(date);
+  } else {
+    selectedDate.value = date;
+  }
 };
 
 const saveWorkout = () => {
@@ -76,6 +89,11 @@ const saveWorkout = () => {
 
 <style>
 :root {
-  --van-primary-color: #1989fa;
+  --van-primary-color: #3c8ee0;
+  --van-toast-default-width: 150px;
+}
+
+.van-toast--success {
+  box-shadow: 0 0 2px 0 var(--van-toast-text-color);
 }
 </style>
