1fa0ed143e5beb9401cab999888c62cc0a7d2b7b
Max <scorpiomax90@gmail.com>
2026-08-01 21:20:36 +0300

    Install nuxt

---
+++ app/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtRouteAnnouncer />
+
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'LiftTracker',
+  meta: [
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1.0',
+    },
+  ],
+});
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>LiftTracker</div>
+</template>
